cd83b9710b0c5a1caca872153fcc8e3787c6c173
Phun Peeticharoenthum <phunphun484@gmail.com>
2023-06-23 15:44:42 +0700

    FIX: delete backup files bug

--- Editor/PlayAssetPackBackup.cs
+++ Editor/PlayAssetPackBackup.cs
@@ -36,11 +36,7 @@ namespace Khepri.PlayAssetDelivery.Editor
             Debug.Log($"Restore All backup files to : {directory} successfully.");
             
             foreach (string filePath in files)
-            {
-                string fileName = Path.GetFileName(filePath);
-                string targetFilePath = Path.Combine(directory, fileName);
-                File.Delete(targetFilePath);
-            }
+                File.Delete(filePath);
         }
     }
 }
